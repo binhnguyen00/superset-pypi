1cdc270a4ac701ed9533d02c96a79f2ed5bb6f03
marci <jackjack2000.kahp@gmail.com>
2024-11-24 13:54:51 +0700

    update shell script

--- superset.sh
+++ superset.sh
@@ -1,5 +1,11 @@
+#!/bin/bash
+
+ENV_FILE=".env"
+
 function show_help() {
   echo """
+!IMPORTANT: This script can only work on MacOS. Windows will be supported in the near future
+
 Usage: Superset commands
   ./database.sh [COMMAND] [OPTION]
 
@@ -9,7 +15,17 @@ Run Superset
 Active Python Virtual Environment
   ./superset.sh active-env
 
-  """
+Export Flask App
+  ./superset.sh export-flask-app
+
+!IMPORTANT: Best to run this command only once or twice
+Generate Secret Key
+  ./superset.sh generate-secret-key  
+
+!IMPORTANT: Best to run this command after active-env
+Export Secret Key
+  ./superset.sh export-secret-key
+"""
 }
 
 function run() {
@@ -21,6 +37,31 @@ function run() {
 
 function export_flask_app() {
   export FLASK_APP=superset
+}
+
+function generate_secret_key() {
+  SECRET_KEY=$(openssl rand -base64 32 | tr -d '\n')
+  echo "SUPERSET_SECRET_KEY=${SECRET_KEY}" >> "${ENV_FILE}"
+  echo $SECRET_KEY
+}
+
+function export_secret_key() {
+  if [ -f ".env" ]; then
+    SECRET_KEY=$(tail -n 1 .env | awk -F'=' '{print $2}')
+    if [ -n "$SECRET_KEY" ]; then
+      export SUPERSET_SECRET_KEY="$SECRET_KEY"
+      echo """
+=================================================
+SUPERSET_SECRET_KEY: 
+    $SUPERSET_SECRET_KEY
+=================================================
+      """
+    else
+      echo "No SUPERSET_SECRET_KEY found"
+    fi
+  else
+    echo ".env file does not exist."
+  fi
 }
 
 COMMAND=$1;
@@ -38,6 +79,10 @@ elif [ "$COMMAND" = "active-env" ] ; then
   exec poetry shell
 elif [ "$COMMAND" = "export-flask-app" ] ; then
   export_flask_app
+elif [ "$COMMAND" = "export-secret-key" ] ; then
+  export_secret_key
+elif [ "$COMMAND" = "generate-secret-key" ] ; then
+  generate_secret_key
 elif [ "$COMMAND" = "help" ] ; then
   show_help
 else
